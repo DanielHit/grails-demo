82759f43f52478c0ecfc505aa322ddd725e0f651
胡磊 <hulei02@meituan.com>
2015-09-07 14:19:51 +0800

    change return

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,12 @@
 
 function start(){
     console.log("start do somethings");
-    return("stared successfully");
+    return "stared successfully";
 }
 
 function upload(){
     console.log("upload to do somethings");
-    return("upload successfully");
+    return "upload successfully";
 }
 exports.start = start;
 exports.upload = upload;
